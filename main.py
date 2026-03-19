b13aca94c20eac930ecbe385aed7db86411f5688
Твоя Фамилия Имя <pakhrudinov_daniil@mail.ru>
2026-03-19 11:34:19 +0300

    Добавлена функция деления

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if b == 0:
+        return "Деление на ноль!"
+    return a / b
+
 print("2 + 3 =", add(2, 3))
 print("5 - 2 =", subtract(5, 2))
 print("4 * 3 =", multiply(4, 3))
+print("10 / 2 =", divide(10, 2))
